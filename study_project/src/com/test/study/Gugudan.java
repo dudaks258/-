b587bcd2d884b130525293f6d1ee646c48180def
sw04 <sw04@sw04-PC>
2017-11-30 19:43:21 +0900

    commit Gigidan.java

---
+++ study_project/src/com/test/study/Gugudan.java
@@ -0,0 +1,17 @@
+package com.test.study;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+
+		for (int dno = 2; dno < 10; dno++) {
+			System.out.println(dno + "단 입니다.");
+			for (int gop = 1; gop < 10; gop++) {
+				System.out.println(dno + "*" + gop + "=" + dno * gop);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
